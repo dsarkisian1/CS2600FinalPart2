77fce56d84e74f2a802c4c10c262496a43f1621a
David Sarkisian <de.sarkisian@gmail.com>
2021-12-10 18:27:38 +0000

    Added the function enableRawMode, added a line to call the function from main, and included new headers

--- kilo.c
+++ kilo.c
@@ -1,3 +1,13 @@
+#include <termios.h>
+#include <unistd.h>
+
+void enableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
   enableRawMode();
   char c;
